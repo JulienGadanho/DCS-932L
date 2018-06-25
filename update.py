1b1ea91c09286ae361815793e9796b44821647fc
root <root@localhost.localdomain>
2018-06-25 11:57:28 +0200

    release

---
+++ update.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import time
+
+print "Loading..."
+time.sleep(1)
+print "5%"
+time.sleep(1)
+print "10%"
+time.sleep(1)
+print "20%"
+time.sleep(1)
+print "30%"
+time.sleep(1)
+print "40%"
+time.sleep(1)
+print "50%"
+time.sleep(1)
+print "60%"
+time.sleep(1)
+print "70%"
+time.sleep(1)
+print "80%"
+time.sleep(1)
+print "90%"
+time.sleep(1)
+print "100%"
+time.sleep(3)
+print "Update ok"
